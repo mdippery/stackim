6d46a3437e1958e23924ff333350341f05fd83fd
Michael Dippery <michael@monkey-robot.com>
2017-09-08 16:09:36 -0700

    Create new tags using PUT request

--- src/stackim/core.clj
+++ src/stackim/core.clj
@@ -1,8 +1,10 @@
 (ns stackim.core
   (:gen-class)
-  (:require [compojure.core :refer :all]
+  (:require [clojure.string :as str]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [org.httpkit.server :as http]
+            [ring.middleware.params :as ring]
             [selmer.parser :as selmer]
             [stackim.db :as db]
             [stackim.tags :as tags]))
@@ -14,8 +16,21 @@
 (defn stack-url [id]
   (str "http://stackoverflow.com/users/" id))
 
+(defn int? [n]
+  (try
+    (do
+      (Integer/parseInt n)
+      true)
+    (catch NumberFormatException e false)))
+
 (defn redirect [to]
   {:status 301 :headers {"Location" to} :body ""})
+
+(defn oops [code body]
+  {:status code :body body})
+
+(defn ok [body]
+  {:status 200 :body body})
 
 
 (defn get-tag [tag]
@@ -25,12 +40,23 @@
         (-> tag tags/stack-id stack-url redirect))
       ({:status 404 :body (str "No tag for " tag)})))
 
+(defn put-tag [tag id]
+  (cond
+    (nil? id) (oops 400 "PUT request without 'stackid' parameter")
+    (not (tags/valid? tag)) (oops 403 "Shortened URL may only contain alphanumeric characters")
+    (tags/exists? tag) (oops 409 (str "Tag '" tag "' is already in use"))
+    (not (int? id)) (oops 403 (str "Invalid Stack Overflow ID: '" id "'"))
+    :else (do
+            (tags/insert tag id)
+            (ok "OK"))))
+
 (defroutes stackim
   (GET "/" [] (selmer/render-file "templates/home.html" {}))
   (GET "/:tag" [tag] (get-tag tag))
+  (PUT "/:tag" req (put-tag (:tag (:params req)) (get (:params req) "stackid")))
   (route/resources "/"))
 
 (defn -main []
   (db/create-tables!)
   (println "Starting server on port" port)
-  (http/run-server stackim {:port port}))
+  (http/run-server (ring/wrap-params stackim) {:port port}))
