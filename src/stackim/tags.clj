d3bbeb7db788e686e97dfecae112fc53e5619169
Michael Dippery <michael@monkey-robot.com>
2017-09-08 14:35:56 -0700

    Interact with underlying database using tags module

---
+++ src/stackim/tags.clj
@@ -0,0 +1,19 @@
+(ns stackim.tags
+  (:require [clojure.java.jdbc :as jdbc]
+            [stackim.db :as db]))
+
+
+(defn insert [tag id]
+  (jdbc/insert! db/db :tags {:name tag :stack_id id}))
+
+(defn stack-id [tag]
+  (-> (jdbc/query db/db ["SELECT stack_id FROM tags WHERE name = ?" tag]) first :stack_id))
+
+(defn record-visit [tag hostname]
+  (jdbc/execute! db/db ["INSERT INTO hits (tag_id, hostname) VALUES ((SELECT id FROM tags WHERE name = ?), ?)" tag hostname]))
+
+(defn exists? [tag]
+  (-> tag stack-id nil?))
+
+(defn valid? [tag]
+  (not (nil? (re-matches #"[a-zA-Z0-9]+" tag))))
