d6608e2a1c0b77aad3d2aeadba32f247c20c4e52
Michael Dippery <michael@monkey-robot.com>
2017-09-08 09:36:59 -0700

    Organize source code

--- src/stackim/core.clj
+++ src/stackim/core.clj
@@ -6,14 +6,13 @@
             [selmer.parser :refer [render-file]]))
 
 
-(defroutes stackim
-  (GET "/" [] (render-file "templates/home.html" {}))
-  (route/resources "/"))
-
-
 (def port
   (Integer/parseInt (or (System/getenv "PORT") "5000")))
 
+
+(defroutes stackim
+  (GET "/" [] (render-file "templates/home.html" {}))
+  (route/resources "/"))
 
 (defn -main []
   (run-server stackim {:port port}))
