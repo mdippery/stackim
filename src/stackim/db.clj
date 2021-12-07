382b10f4fc01759001b683e0de53bd36ed3227fb
Michael Dippery <michael@monkey-robot.com>
2021-12-07 10:17:21 -0800

    Return empty DB options in Lambda
    When running in a Lambda, we do not want to return AWS credentials; the
    Lambda itself will have an attached IAM role that allows access to
    DynamoDB. Thus, if the AWS_ACCESS_KEY_ID envvar is not set, simply
    return an empty set of DB options.

--- src/stackim/db.clj
+++ src/stackim/db.clj
@@ -1,12 +1,21 @@
 (ns stackim.db)
 
+
+(defn- env? [var]
+  (not (nil? (System/getenv var))))
+
 (def region
   (or (System/getenv "AWS_DEFAULT_REGION") "us-west-1"))
 
-(def db-options
+(def ^:private local-options
   {:access-key (System/getenv "AWS_ACCESS_KEY_ID")
    :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")
    :endpoint (str "https://dynamodb." region ".amazonaws.com")})
+
+(def ^:private lambda-options {})
+
+(def db-options
+  (if (env? "AWS_ACCESS_KEY_ID") local-options lambda-options))
 
 (def table
   (or (System/getenv "STACKIM_TAGS_TABLE") "stackim-tags"))
