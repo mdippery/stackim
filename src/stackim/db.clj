a3fda1cacd9ab132c025b1145a2b96a62b0232d0
Michael Dippery <michael@monkey-robot.com>
2021-12-07 00:37:15 -0800

    Retrieve DynamoDB table name from environment

--- src/stackim/db.clj
+++ src/stackim/db.clj
@@ -3,3 +3,6 @@
 (def db-options
   {:access-key (System/getenv "AWS_ACCESS_KEY_ID")
    :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")})
+
+(def table
+  (or (System/getenv "STACKIM_TAGS_TABLE") "stackim-tags"))
