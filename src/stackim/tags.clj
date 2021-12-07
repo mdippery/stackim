7013fece708dd5c6bcd0d25fff9d60fd4c31d2b2
Michael Dippery <michael@monkey-robot.com>
2021-12-07 01:14:30 -0800

    Prevent duplicates from being added to database

--- src/stackim/tags.clj
+++ src/stackim/tags.clj
@@ -3,18 +3,20 @@
             [stackim.db :as db]))
 
 
-(defn insert [tag id]
-  (far/put-item db/db-options db/table {:Alias tag :ProfileID id}))
-
 (defn stack-id [tag]
   (:ProfileID (far/get-item db/db-options db/table {:Alias tag})))
-
-(defn record-visit [tag referer]
-  ; (jdbc/execute! db/db ["INSERT INTO hits (tag_id, referer) VALUES ((SELECT id FROM tags WHERE name = ?), ?)" tag referer]))
-  nil)
 
 (defn exists? [tag]
   (-> tag stack-id nil? not))
 
 (defn valid? [tag]
   (not (nil? (re-matches #"[a-zA-Z0-9]+" tag))))
+
+(defn record-visit [tag referer]
+  ; (jdbc/execute! db/db ["INSERT INTO hits (tag_id, referer) VALUES ((SELECT id FROM tags WHERE name = ?), ?)" tag referer]))
+  nil)
+
+(defn insert [tag id]
+  (if (exists? tag)
+    false
+    (nil? (far/put-item db/db-options db/table {:Alias tag :ProfileID id}))))
