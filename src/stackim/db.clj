6cbd4d1183e47d402fcf9e386ae1c3993d7dbb43
Michael Dippery <michael@monkey-robot.com>
2021-12-07 01:00:20 -0800

    Configure DynamoDB region using environment variable

--- src/stackim/db.clj
+++ src/stackim/db.clj
@@ -1,8 +1,12 @@
 (ns stackim.db)
 
+(def region
+  (or (System/getenv "AWS_DEFAULT_REGION") "us-west-1"))
+
 (def db-options
   {:access-key (System/getenv "AWS_ACCESS_KEY_ID")
-   :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")})
+   :secret-key (System/getenv "AWS_SECRET_ACCESS_KEY")
+   :endpoint (str "https://dynamodb." region ".amazonaws.com")})
 
 (def table
   (or (System/getenv "STACKIM_TAGS_TABLE") "stackim-tags"))
