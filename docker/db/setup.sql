a10b5a46367ae5fc6c2a3baf152d331928707420
Michael Dippery <michael@monkey-robot.com>
2025-08-06 16:25:32 -0700

    Create appropriate indexes in database
    This will improve performance...not that performance is a huge concern
    for this application, but still.

--- docker/db/setup.sql
+++ docker/db/setup.sql
@@ -4,6 +4,7 @@ CREATE TABLE tags (
     stack_id   integer                     NOT NULL,
     created_at timestamp with time zone    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+CREATE INDEX tags_name_idx ON tags (name);
 
 CREATE TABLE hits (
     id        serial                       PRIMARY KEY,
@@ -11,3 +12,5 @@ CREATE TABLE hits (
     timestamp timestamp with time zone     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     referer   varchar(1024)                NOT NULL
 );
+CREATE INDEX hits_tag_id_idx ON hits (tag_id);
+CREATE INDEX hits_timestamp_idx ON hits (timestamp);
