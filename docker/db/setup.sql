d92b5d3d8a2fcd0839c0b44601b10002bd55567a
Michael Dippery <michael@monkey-robot.com>
2025-08-06 12:44:31 -0700

    Timestamp columns should store time zones
    Oops! Forgot to do this before.

--- docker/db/setup.sql
+++ docker/db/setup.sql
@@ -1,13 +1,13 @@
 CREATE TABLE tags (
-    id         serial       PRIMARY KEY,
-    name       varchar(256) NOT NULL UNIQUE,
-    stack_id   integer      NOT NULL,
-    created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id         serial                      PRIMARY KEY,
+    name       varchar(256)                NOT NULL UNIQUE,
+    stack_id   integer                     NOT NULL,
+    created_at timestamp with time zone    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE hits (
-    id        serial        PRIMARY KEY,
-    tag_id    integer       REFERENCES tags (id),
-    timestamp timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    referer   varchar(1024) NOT NULL
+    id        serial                       PRIMARY KEY,
+    tag_id    integer                      REFERENCES tags (id),
+    timestamp timestamp with time zone     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    referer   varchar(1024)                NOT NULL
 );
