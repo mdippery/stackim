68a5e7df73f85878a2c219c0a4fd7f892421c8cf
Michael Dippery <michael@monkey-robot.com>
2025-08-09 15:44:07 -0700

    Explictly use public schema
    For some reason, this was necessary when I moved to a new database host
    -- no idea why.

--- src/stackim/tags.clj
+++ src/stackim/tags.clj
@@ -4,13 +4,13 @@
 
 
 (defn insert [tag id]
-  (jdbc/insert! db/db :tags {:name tag :stack_id id}))
+  (jdbc/insert! db/db :public.tags {:name tag :stack_id id}))
 
 (defn stack-id [tag]
-  (-> (jdbc/query db/db ["SELECT stack_id FROM tags WHERE name = ?" tag]) first :stack_id))
+  (-> (jdbc/query db/db ["SELECT stack_id FROM public.tags WHERE name = ?" tag]) first :stack_id))
 
 (defn record-visit [tag referer]
-  (jdbc/execute! db/db ["INSERT INTO hits (tag_id, referer) VALUES ((SELECT id FROM tags WHERE name = ?), ?)" tag referer]))
+  (jdbc/execute! db/db ["INSERT INTO public.hits (tag_id, referer) VALUES ((SELECT id FROM public.tags WHERE name = ?), ?)" tag referer]))
 
 (defn exists? [tag]
   (-> tag stack-id nil? not))
