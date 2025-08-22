3bb0c0c548d0854ea8a1b4250570bbee2a944d3a
Michael Dippery <michael@monkey-robot.com>
2025-08-22 09:25:48 -0700

    Improve test output
    By describing tests using the `(testing)` function, we can improve the
    output of test failures to help us immediately see what went wrong.

--- test/stackim/core_test.clj
+++ test/stackim/core_test.clj
@@ -12,124 +12,134 @@
 
 (defn- jdbc-execute! [opts query] true)
 
-(deftest test-port-with-envvar
-  (binding [core/*env* {"PORT" "8080"}]
-    (is (= (core/port) 8080))))
+(deftest port-configuration
+  (testing "port configuration"
+    (testing "with environment variable"
+      (binding [core/*env* {"PORT" "8080"}]
+        (is (= (core/port) 8080))))
 
-(deftest test-port-wo-envvar
-  (is (= (core/port) 5000)))
+    (testing "without environment variable"
+      (is (= (core/port) 5000)))))
 
-(deftest test-canonical-port-with-envvar
-  (binding [core/*env* {"CANONICAL_PORT" "8080"}]
-    (is (= (core/canonical-port) 8080))))
+(deftest canonical-port
+  (testing "canonical port configuration"
+    (testing "with environment variable"
+      (binding [core/*env* {"CANONICAL_PORT" "8080"}]
+        (is (= (core/canonical-port) 8080))))
 
-(deftest test-canonical-port-wo-envvar
-  (is (= (core/canonical-port) 443)))
+    (testing "without environment variable"
+      (is (= (core/canonical-port) 443)))))
 
-(deftest test-canonical-host-with-envvar
-  (binding [core/*env* {"CANONICAL_HOST" "stack.im"}]
-    (is (= (core/canonical-host) "stack.im:443"))))
+(deftest canonical-host
+  (testing "canonical host configuration"
+    (testing "without environment variable"
+      (binding [core/*env* {"CANONICAL_HOST" "stack.im"}]
+        (is (= (core/canonical-host) "stack.im:443"))))
 
-(deftest test-canonical-host-wo-envvar
-  (binding [core/*env* {"CANONICAL_PORT" "5000"}]
-    (is (= (core/canonical-host) "localhost:5000"))))
+    (testing "with environment variable"
+      (binding [core/*env* {"CANONICAL_PORT" "5000"}]
+        (is (= (core/canonical-host) "localhost:5000"))))))
 
-(deftest test-canonical-proto-with-header
-  (let [request (-> (mock/request :get "/")
-                    (mock/header "X-Forwarded-Proto" "https")
-                    (mock/header "Content-Type" "html"))]
-    (is (= (core/canonical-proto request) "https"))))
+(deftest canonical-protocol
+  (testing "canonical protocol"
+    (testing "with header set"
+      (let [request (-> (mock/request :get "/")
+                        (mock/header "X-Forwarded-Proto" "https")
+                        (mock/header "Content-Type" "html"))]
+        (is (= (core/canonical-proto request) "https"))))
 
-(deftest test-canonical-proto-wo-header
-  (let [request (-> (mock/request :get "/")
-                    (mock/header "Content-Type" "html"))]
-    (is (= (core/canonical-proto request) "http"))))
+    (testing "without header set"
+      (let [request (-> (mock/request :get "/")
+                        (mock/header "Content-Type" "html"))]
+        (is (= (core/canonical-proto request) "http"))))))
 
-(deftest test-stack-url
-  (is (= (core/stack-url 28804) "http://stackoverflow.com/users/28804")))
+(deftest stack-url
+  (testing "stack-url"
+    (is (= (core/stack-url 28804) "http://stackoverflow.com/users/28804"))))
 
-(deftest test-valid-int?
-  (is (core/valid-int? "28804"))
-  (is (not (core/valid-int? "-28804")))
-  (is (not (core/valid-int? "100.5")))
-  (is (not (core/valid-int? "")))
-  (is (not (core/valid-int? "foo"))))
+(deftest valid-int?
+  (testing "valid-int?"
+    (testing "with positive integer"
+      (is (core/valid-int? "28804")))
+    (testing "with negative integer"
+      (is (not (core/valid-int? "-28804"))))
+    (testing "with floating point number"
+      (is (not (core/valid-int? "100.5"))))
+    (testing "with empty string"
+      (is (not (core/valid-int? ""))))
+    (testing "with any string"
+      (is (not (core/valid-int? "foo"))))))
 
-(deftest test-redirect
-  (let [res (core/redirect "http://www.example.com/")]
-    (is (= (:status res) 301))
-    (is (= (get (:headers res) "Location") "http://www.example.com/"))
-    (is (= (:body res) ""))))
-
-(deftest test-oops
-  (let [res (core/oops 409 "Tag 'foo' is already in use")]
-    (is (= (:status res) 409))
-    (is (= (:body res) "Tag 'foo' is already in use\n"))))
-
-(deftest test-ok
-  (let [res (core/ok "Success!")]
-    (is (= (:status res) 200))
-    (is (= (:body res) "Success!\n"))))
-
-(deftest test-get-tag
-  (with-redefs [jdbc/query jdbc-query
-                jdbc/insert! jdbc-insert!
-                jdbc/execute! jdbc-execute!]
-    (testing "tag exists"
-      (let [res (core/get-tag "mpd")]
+(deftest redirect
+  (testing "redirect"
+    (testing "redirects from tag"
+      (let [res (core/redirect "http://www.example.com/")]
         (is (= (:status res) 301))
-        (is (= (get (:headers res) "Location") "http://stackoverflow.com/users/28804"))
-        (is (= (:body res) ""))))
-    (testing "tag does not exist"
-      (let [res (core/get-tag "foo")]
-        (is (= (:status res) 404))
-        (is (nil? (get (:headers res) "Location")))
-        (is (= (:body res) "No tag for foo\n"))))))
+        (is (= (get (:headers res) "Location") "http://www.example.com/"))
+        (is (= (:body res) ""))))))
 
-(deftest test-put-tag
-  (with-redefs [jdbc/query jdbc-query
-                jdbc/insert! jdbc-insert!
-                jdbc/execute! jdbc-execute!]
-    (testing "no stackid"
-      (let [res (core/put-tag "mpd" nil)]
-        (is (= (:status res) 400))
-        (is (= (:body res) "PUT request without 'stackid' parameter\n"))))
-    (testing "invalid tag"
-      (let [res (core/put-tag "foo@bar" "28804")]
-        (is (= (:status res) 403))
-        (is (= (:body res) "Shortened URL may only contain alphanumeric characters\n"))))
-    (testing "tag exists"
-      (let [res (core/put-tag "mpd" "28805")]
+(deftest oops
+  (testing "oops"
+    (testing "returns an error status and string"
+      (let [res (core/oops 409 "Tag 'foo' is already in use")]
         (is (= (:status res) 409))
-        (is (= (:body res) "Tag 'mpd' is already in use\n"))))
-    (testing "invalid integer"
-      (let [res (core/put-tag "foo" "foo")]
-        (is (= (:status res) 403))
-        (is (= (:body res) "Invalid Stack Overflow ID: 'foo'\n"))))
-    (testing "negative integer"
-      (let [res (core/put-tag "foo" "-28804")]
-        (is (= (:status res) 403))
-        (is (= (:body res) "Invalid Stack Overflow ID: '-28804'\n"))))
-    (testing "valid request"
-      (let [res (core/put-tag "foo" "28805")]
+        (is (= (:body res) "Tag 'foo' is already in use\n"))))))
+
+(deftest ok
+  (testing "ok"
+    (testing "returns successfully"
+      (let [res (core/ok "Success!")]
         (is (= (:status res) 200))
-        (is (= (:body res) "OK\n"))))))
+        (is (= (:body res) "Success!\n"))))))
 
-(deftest test-get-homepage
-  (let [resp (core/stackim (mock/request :get "/"))]
-    (is (= (:status resp) 200))))
+(deftest get-tag
+  (testing "get-tag"
+    (with-redefs [jdbc/query jdbc-query
+                  jdbc/insert! jdbc-insert!
+                  jdbc/execute! jdbc-execute!]
+      (testing "tag exists"
+        (let [res (core/get-tag "mpd")]
+          (is (= (:status res) 301))
+          (is (= (get (:headers res) "Location") "http://stackoverflow.com/users/28804"))
+          (is (= (:body res) ""))))
+      (testing "tag does not exist"
+        (let [res (core/get-tag "foo")]
+          (is (= (:status res) 404))
+          (is (nil? (get (:headers res) "Location")))
+          (is (= (:body res) "No tag for foo\n")))))))
 
-(deftest test-get-tag
-  (with-redefs [jdbc/query jdbc-query
-                jdbc/insert! jdbc-insert!
-                jdbc/execute! jdbc-execute!]
-    (testing "tag exists"
-      (let [resp (core/stackim (mock/request :get "/mpd"))]
-        (is (= (:status resp) 301))
-        (is (= (get (:headers resp) "Location") "http://stackoverflow.com/users/28804"))
-        (is (= (:body resp) ""))))
-    (testing "tag does not exist"
-      (let [resp (core/stackim (mock/request :get "/foo"))]
-        (is (= (:status resp) 404))
-        (is (nil? (get (:headers resp) "Location")))
-        (is (= (:body resp) "No tag for foo\n"))))))
+(deftest put-tag
+  (testing "put-tag"
+    (with-redefs [jdbc/query jdbc-query
+                  jdbc/insert! jdbc-insert!
+                  jdbc/execute! jdbc-execute!]
+      (testing "no stackid"
+        (let [res (core/put-tag "mpd" nil)]
+          (is (= (:status res) 400))
+          (is (= (:body res) "PUT request without 'stackid' parameter\n"))))
+      (testing "invalid tag"
+        (let [res (core/put-tag "foo@bar" "28804")]
+          (is (= (:status res) 403))
+          (is (= (:body res) "Shortened URL may only contain alphanumeric characters\n"))))
+      (testing "tag exists"
+        (let [res (core/put-tag "mpd" "28805")]
+          (is (= (:status res) 409))
+          (is (= (:body res) "Tag 'mpd' is already in use\n"))))
+      (testing "invalid integer"
+        (let [res (core/put-tag "foo" "foo")]
+          (is (= (:status res) 403))
+          (is (= (:body res) "Invalid Stack Overflow ID: 'foo'\n"))))
+      (testing "negative integer"
+        (let [res (core/put-tag "foo" "-28804")]
+          (is (= (:status res) 403))
+          (is (= (:body res) "Invalid Stack Overflow ID: '-28804'\n"))))
+      (testing "valid request"
+        (let [res (core/put-tag "foo" "28805")]
+          (is (= (:status res) 200))
+          (is (= (:body res) "OK\n")))))))
+
+(deftest get-homepage
+  (testing "/"
+    (testing "returns 200 OK"
+      (let [resp (core/stackim (mock/request :get "/"))]
+        (is (= (:status resp) 200))))))
--- test/stackim/tags_test.clj
+++ test/stackim/tags_test.clj
@@ -11,25 +11,41 @@
 
 (defn- jdbc-execute! [opts query] true)
 
-(deftest test-insert
-  (with-redefs [jdbc/insert! jdbc-insert!]
-    (is (tags/insert "mpd" 28804))))
+(deftest insert
+  (testing "insert"
+    (with-redefs [jdbc/insert! jdbc-insert!]
+      (is (tags/insert "mpd" 28804)))))
 
-(deftest test-record-visit
-  (with-redefs [jdbc/execute! jdbc-execute!]
-    (is (tags/record-visit "mpd" "http://www.example.com/"))))
+(deftest record-visit
+  (testing "record-visit"
+    (with-redefs [jdbc/execute! jdbc-execute!]
+      (is (tags/record-visit "mpd" "http://www.example.com/")))))
 
-(deftest test-exists?
-  (with-redefs [jdbc/query jdbc-query]
-    (is (tags/exists? "mpd"))
-    (is (not (tags/exists? "foo")))))
+(deftest exists?
+  (testing "exists?"
+    (with-redefs [jdbc/query jdbc-query]
+      (is (tags/exists? "mpd"))
+      (is (not (tags/exists? "foo"))))))
 
-(deftest test-valid?
-  (is (tags/valid? "foo"))
-  (is (tags/valid? "123"))
-  (is (tags/valid? "foo123"))
-  (is (tags/valid? "123foo"))
-  (is (tags/valid? "f1o2o3"))
-  (is (tags/valid? "1f2o3o"))
-  (is (not (tags/valid? "")))
-  (is (not (tags/valid? "foo@1"))))
+(deftest valid?
+  (testing "valid?"
+    (testing "is valid"
+      (testing "an alphabetic string"
+        (is (tags/valid? "foo")))
+      (testing "a numeric string"
+        (is (tags/valid? "123")))
+      (testing "an alphanumeric string"
+        (testing "beginning with alpha"
+          (is (tags/valid? "foo123")))
+        (testing "beginning with numbers"
+          (is (tags/valid? "123foo")))
+        (testing "with alternating character classes"
+          (testing "beginning with an alpha"
+            (is (tags/valid? "f1o2o3")))
+          (testing "beginning with a number"
+            (is (tags/valid? "1f2o3o"))))))
+    (testing "is not valid"
+      (testing "an empty string"
+        (is (not (tags/valid? ""))))
+      (testing "a string with symbols"
+        (is (not (tags/valid? "foo@1")))))))
